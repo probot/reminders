54b04ad5f858ab0aa1a0ad0b5072f86d3e24affe
Jamie Jones <jbjonesjr@github.com>
2017-07-16 23:16:51 -0400

    create first valid/invalid tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ const expect = require('expect');
 const {createRobot} = require('probot');
 const plugin = require('..');
 const commentEvent = require('./fixtures/issue_comment.created');
+const Freeze = require('../lib/freeze.js');
+const moment = require('moment');
 
 describe('PRobot-Snooze ', () => {
   let robot;
@@ -176,5 +178,74 @@ perform: true
       number: '2',
       body: ':wave: @baxterthehacker, Hey, we\'re back awake!'
     });
+  });
+
+  it('test valid comments', async () => {
+    const validMessages = [
+      {msg:'@probot, snooze this message until 08/01/2018. Then remind me about this test', props:{assignee: 'baxterthehacker', message: 'Hey, we\'re back awake!', unfreezeMoment: moment(new Date('08/01/2018 12:00'))}}
+/*      {msg:'snooze this issue', props:{}} // ,
+      {msg:'snooze this thread until next Tuesday',
+      {msg:'snooze this thread until the 25th',
+      {msg:'snooze this thread for a week',
+      {msg:'snooze this thread til next Friday',
+      {msg:'snooze this until tomorrow at noon',
+      {msg:'snooze this until tomorrow at 2:00pm',
+    /* Various date format parsing */
+/*      {msg:'snooze until 07/11/17 at 12:00am',
+      {msg:'snooze until 07/11/17 at 2pm',
+      {msg:'snooze until 07/11/17 at 2:00pm',
+      {msg:'snooze until 07/11/17 at 2:30pm',
+      {msg:'snooze until 07/11/17 at 14:00',
+      {msg:'snooze until 07/11/17 at 14:30',
+      {msg:'snooze until 07/11/17',
+      {msg:'freeze until 07/11/17 14:00',
+    /* Full text */
+/*      {msg:'So i\'m out of office for the next three weeks. I\'m going to snooze this until I get back.',
+      'So i\'m out of office for the next three weeks. I\'m going to snooze this until I get back on 07/21/17.',
+      {msg:'Thanks for looking into this.\n\nSo i\'m out of office for the next three weeks. I\'m going to snooze this until I get back on 07/21/17.',
+    // With open comment
+      {msg:'snooze until 07/11/17 at 14:00, and remind me to bug Seth again',
+      {msg:'snooze until 07/11/17 at 14:00 to bug Seth',
+      {msg:'snooze until 07/11/17 at 14:00, and "bug Seth"',
+      {msg:'hey @probot, snooze this issue'}
+*/
+    ];
+
+    const freeze = new Freeze(github, {});
+
+    validMessages.forEach(obj => {
+      const comment = {
+        user: {
+          login: 'baxterthehacker'
+        },
+        body:obj.msg
+      };
+      expect(freeze.freezable(comment)).toBe(true);
+      expect(freeze.propsHelper(comment.user.login, comment.body)).toEqual(obj.props);
+    });
+  });
+
+  it('test invalid comments', async () => {
+    const invalidMessages = [
+      {msg:'it\'s really cold out tonight, hope you don\'t freeze', props:{}}
+    ];
+    const freeze = new Freeze(github, {});
+
+    invalidMessages.forEach(obj => {
+      const comment = {
+        user: {
+          login: 'baxterthehacker'
+        },
+        body:obj.msg
+      };
+      expect(freeze.freezable(comment)).toBe(false);
+    });
+  });
+
+  it('these tests are faulty edge cases, and here for reference', async () => {
+    // Chrono-node
+    const badChrono = ['snooze this thread until July 25th',
+      'snooze this thread a week'];
+    expect(badChrono).toExist();
   });
 });
