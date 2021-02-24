ef5af89673e7537021df742182564c48dc343b23
Jamie Jones <jbjonesjr@github.com>
2021-02-24 07:37:43 +0000

    fixing entrypoint

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const reminders = require('./lib/reminders')
 
 module.exports = robot => {
   createScheduler(robot, {interval: 15 * 60 * 1000})
+  // 'issue_comment.created', 'issues.opened', 'pull_request.opened'
+  // new Command(name, callback)
   commands(robot, 'remind', reminders.set)
+  // call reminder checks on cron run
   robot.on('schedule.repository', reminders.check)
 }
