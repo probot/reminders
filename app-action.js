53a8d2091a7e40d5b32c8a3f8011000026187536
Jamie Jones <jbjonesjr@github.com>
2021-02-27 19:24:48 +0000

    building out slash command work

--- app-action.js
+++ app-action.js
@@ -7,11 +7,14 @@ const reminders = require('./lib/reminders')
 
 module.exports = robot => {
  
-  // 'issue_comment.created', 'issues.opened', 'pull_request.opened'
-  // new Command(name, callback)
-  commands(robot, 'remind', reminders.set)
-
-
-  // call reminder checks on cron run
+//  robot.on(['issue_comment.created', 'issues.opened', 'pull_request.opened'], testCommand)
+// commands may be a reusable pattern to pass the right octokit downstream for probot and actions usage  
+commands(robot, 'remind', reminders.set)
   robot.on('schedule', reminders.check)
+}
+
+testCommand = function(context){
+  /^\/([\w]+)\b *(.*)?$/m
+  //look for remind
+  reminders.set(context);
 }
