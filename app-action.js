7881bba843a0cb363932d0df59ad04dcd5d29ff5
Jamie Jones <jbjonesjr@github.com>
2021-02-27 17:09:44 +0000

    embrace that action life

--- app-action.js
+++ app-action.js
@@ -3,24 +3,17 @@ process.env.TZ = 'UTC'
 
 const commands = require('probot-commands')
 const reminders = require('./lib/reminders')
+const Octokit = require('octokit/action')
+
 
 module.exports = robot => {
-
-  /*
-{o
-
-   const repo = this.payload.repository;
-        if (!repo) {
-            throw new Error("context.repo() is not supported for this webhook event.");
-        }
-        return Object.assign({
-            owner: repo.owner.login || repo.owner.name,
-            repo: repo.name,
-        }, object);
-        */
+ 
   // 'issue_comment.created', 'issues.opened', 'pull_request.opened'
   // new Command(name, callback)
   commands(robot, 'remind', reminders.set)
+
+  const octokit = new Octokit();
+
   // call reminder checks on cron run
-  robot.on('schedule', reminders.check)
+  robot.on('schedule', reminders.check, octokit)
 }
