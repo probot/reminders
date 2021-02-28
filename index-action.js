006869c087ee0a3d84b4a4214aad4bead2a322b1
Jamie Jones <jbjonesjr@github.com>
2021-02-28 16:04:50 +0000

    move to a single index model, pivoting on Actions rutime

--- index-action.js
+++
@@ -1,4 +0,0 @@
-const { run } = require('@probot/adapter-github-actions')
-const app = require('./app-action')
-
-run(app)
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const { run } = require('probot')
-const app = require('./app')
+const { run } = require('@probot/adapter-github-actions')
+const actionApp = require('./app-action')
+const probotApp = require('./app')
 
-run(app)
+if (provess.env.GITHUB_ACTIONS) {
+    run(actionApp).catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+} else {
+    run(probotApp).catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+}
