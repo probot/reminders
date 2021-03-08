27a8203a964519437fade00e87a004b9060967f6
Jamie Jones <jbjonesjr@github.com>
2021-03-08 03:22:05 +0000

    standard --fix

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const actionApp = require('./app-action')
 const probotApp = require('./app')
 
 if (process.env.GITHUB_ACTIONS) {
-    run(actionApp).catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  run(actionApp).catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
 } else {
-    probotRun(probotApp).catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  probotRun(probotApp).catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
 }
