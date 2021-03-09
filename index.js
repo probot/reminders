a31e43582ab78c3d5bf56678b0c55e4b3ca2f4ef
Jamie Jones <jbjonesjr@github.com>
2021-03-09 04:42:00 +0000

    streamline probot usage

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const { run } = require('@probot/adapter-github-actions')
-const { probotRun } = require('probot')
 const actionApp = require('./app-action')
 const probotApp = require('./app')
 
 if (process.env.GITHUB_ACTIONS) {
-  run(actionApp).catch((error) => {
+  require('@probot/adapter-github-actions').run(actionApp).catch((error) => {
     console.error(error)
     process.exit(1)
   })
 } else {
-  probotRun(probotApp).catch((error) => {
-    console.error(error)
+  require('probot').run(probotApp).catch((error) => {
+   console.error(error)
     process.exit(1)
   })
 }
